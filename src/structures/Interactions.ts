afbdd65676b78413a81ae658ced151b3229f6ebe
J_DDev <69683068+JayyDoesDev@users.noreply.github.com>
2024-03-29 17:48:53 -0400

    feat: Type Overloading

--- src/structures/Interactions.ts
+++ src/structures/Interactions.ts
@@ -5,6 +5,7 @@ import { Command, ICommand } from "./Command";
 import { Routes } from "./Routes";
 import { RequestManager } from "./request/RequestManager";
 import { APPLICATION_TYPE } from "./request/typings";
+import { Wrap } from "./utils/Wrap";
 export class Interactions {
   private request: RequestManager;
   private api: string;
@@ -35,38 +36,42 @@ export class Interactions {
   get botToken() {
     return this.options.botToken;
   }
-
-  public async createGlobalCommand(
+  public async createGlobalCommand<T>(command: ICommand): Promise<ApplicationCommand>;
+  public async createGlobalCommand<T>(command: ICommand): Promise<void>;
+  public async createGlobalCommand<T>(
     command: ICommand
-  ): Promise<ApplicationCommand | void> {
-    (await this.request.POST<ApplicationCommand>({
+  ): Promise<T> {
+    return await Wrap(this.request.POST<T>({
       route: Routes.createApplicationCommand(this.options.botID),
       contentType: APPLICATION_TYPE.JSON,
       data: command,
-    })) as ApplicationCommand;
+    })) as T;
   }
-
-  public async getGlobalCommand(
-    commandId: string
-  ): Promise<ApplicationCommand | void> {
-    (await this.request.GET<ApplicationCommand>({
-      route: Routes.getGlobalApplicationCommand(this.options.botID, commandId),
-      contentType: APPLICATION_TYPE.JSON,
-    })) as ApplicationCommand;
+  public async getGlobalCommand<T>(commandId: string): Promise<ApplicationCommand>;
+  public async getGlobalCommand<T>(commandId: string): Promise<void>;
+  public async getGlobalCommand<T>(
+    commandId: Snowflake
+  ): Promise<T> {
+    return await Wrap(
+      this.request.GET<T>({
+        route: Routes.getGlobalApplicationCommand(this.options.botID, commandId),
+        contentType: APPLICATION_TYPE.JSON,
+      })) as T;
   }
-
-  public async editGlobalCommand(
-    commandId: string,
+  public async editGlobalCommand<T>(commandId: Snowflake, command: ICommand): Promise<ApplicationCommand>;
+  public async editGlobalCommand<T>(commandId: Snowflake, command: ICommand): Promise<void>;
+  public async editGlobalCommand<T>(
+    commandId: Snowflake,
     command: ICommand
-  ): Promise<ApplicationCommand | void> {
-    (await this.request.PATCH<ApplicationCommand>({
-      route: Routes.editGlobalApplicationCommand(this.options.botID, commandId),
-      contentType: APPLICATION_TYPE.JSON,
-      data: command,
-    })) as ApplicationCommand;
+  ): Promise<T> {
+    return await Wrap(
+      this.request.PATCH<T>({
+        route: Routes.editGlobalApplicationCommand(this.options.botID, commandId),
+        contentType: APPLICATION_TYPE.JSON,
+        data: command,
+      })) as T;
   }
-
-  public async deleteGlobalCommand(commandId: string): Promise<void> {
+  public async deleteGlobalCommand(commandId: Snowflake): Promise<void> {
     await this.request.DELETE<ApplicationCommand>({
       route: Routes.deleteGlobalApplicationCommand(
         this.options.botID,
@@ -75,12 +80,14 @@ export class Interactions {
       contentType: APPLICATION_TYPE.JSON,
     });
   }
-
-  public async getGlobalCommands(): Promise<ApplicationCommand[] | void> {
-    (await this.request.GET<ApplicationCommand>({
-      route: Routes.getApplicationCommands(this.options.botID),
-      contentType: APPLICATION_TYPE.JSON,
-    })) as ApplicationCommand;
+  public async getGlobalCommands<T>(): Promise<ApplicationCommand[]>;
+  public async getGlobalCommands<T>(): Promise<void>;
+  public async getGlobalCommands<T>(): Promise<T> {
+    return await Wrap(
+      this.request.GET<T>({
+        route: Routes.getApplicationCommands(this.options.botID),
+        contentType: APPLICATION_TYPE.JSON,
+      })) as T;
   }
 
   public async overwriteGlobalCommands(command: ICommand): Promise<void> {
@@ -90,46 +97,53 @@ export class Interactions {
       data: command,
     });
   }
-
-  public async createGuildCommand(
+  public async createGuildCommand<T>(guildId: Snowflake, command: ICommand): Promise<ApplicationCommand>;
+  public async createGuildCommand<T>(guildId: Snowflake, command: ICommand): Promise<void>;
+  public async createGuildCommand<T>(
     guildId: Snowflake,
     command: ICommand
-  ): Promise<ApplicationCommand | void> {
-    (await this.request.POST<ApplicationCommand>({
-      route: Routes.createGuildApplicationCommand(this.options.botID, guildId),
-      contentType: APPLICATION_TYPE.JSON,
-      data: command,
-    })) as ApplicationCommand;
+  ): Promise<T> {
+    return await Wrap(
+      this.request.POST<T>({
+        route: Routes.createGuildApplicationCommand(this.options.botID, guildId),
+        contentType: APPLICATION_TYPE.JSON,
+        data: command,
+      }
+      )) as T;
   }
-
-  public async getGuildCommands(
+  public async getGuildCommands<T>(guildId: Snowflake): Promise<ApplicationCommand[]>;
+  public async getGuildCommands<T>(guildId: Snowflake): Promise<void>;
+  public async getGuildCommands<T>(
     guildId: Snowflake
-  ): Promise<ApplicationCommand[] | void> {
-    (await this.request.GET<ApplicationCommand>({
-      route: Routes.getGuildApplicationCommands(this.options.botID, guildId),
-      contentType: APPLICATION_TYPE.JSON,
-    })) as unknown as ApplicationCommand[];
+  ): Promise<T> {
+    return await Wrap(
+      this.request.GET<T>({
+        route: Routes.getGuildApplicationCommands(this.options.botID, guildId),
+        contentType: APPLICATION_TYPE.JSON,
+      })) as any;
   }
-
-  public async editGuildCommand(options: {
+  public async editGuildCommand<T>(options: { guildId: Snowflake; commandId: Snowflake; command: ICommand; }): Promise<ApplicationCommand>;
+  public async editGuildCommand<T>(options: { guildId: Snowflake; commandId: Snowflake; command: ICommand; }): Promise<void>;
+  public async editGuildCommand<T>(options: {
     guildId: Snowflake;
-    commandId: string;
+    commandId: Snowflake;
     command: ICommand;
-  }): Promise<ApplicationCommand | void> {
-    (await this.request.PATCH<ApplicationCommand>({
-      route: Routes.editGuildApplicationCommand({
-        applicationId: this.options.botID,
-        guildId: options.guildId,
-        commandId: options.commandId,
-      }),
-      contentType: APPLICATION_TYPE.JSON,
-      data: options.command,
-    })) as ApplicationCommand;
+  }): Promise<T> {
+    return await Wrap(
+      this.request.PATCH<T>({
+        route: Routes.editGuildApplicationCommand({
+          applicationId: this.options.botID,
+          guildId: options.guildId,
+          commandId: options.commandId,
+        }),
+        contentType: APPLICATION_TYPE.JSON,
+        data: options.command,
+      })) as T;
   }
 
   public async deleteGuildCommand(
     guildId: Snowflake,
-    commandId: string
+    commandId: Snowflake
   ): Promise<void> {
     await this.request.DELETE<ApplicationCommand>({
       route: Routes.deleteGuildApplicationCommand({
@@ -154,49 +168,55 @@ export class Interactions {
       data: command,
     });
   }
-
-  public async getGuildCommand(
-    guildID: Snowflake,
-    commandId: string
-  ): Promise<ApplicationCommand | void> {
-    (await this.request.GET<ApplicationCommand>({
-      publicKey: true,
-      route: Routes.getGuildApplicationCommand({
-        applicationId: this.options.botID,
-        guildId: guildID,
-        commandId: commandId,
-      }),
-      contentType: APPLICATION_TYPE.JSON,
-    })) as ApplicationCommand;
+  public async getGuildCommand<T>(guildId: Snowflake, commandId: Snowflake): Promise<ApplicationCommand>;
+  public async getGuildCommand<T>(guildId: Snowflake, commandId: Snowflake): Promise<void>;
+  public async getGuildCommand<T>(
+    guildId: Snowflake,
+    commandId: Snowflake
+  ): Promise<T> {
+    return await Wrap(
+      this.request.GET<T>({
+        publicKey: true,
+        route: Routes.getGuildApplicationCommand({
+          applicationId: this.options.botID,
+          guildId: guildId,
+          commandId: commandId,
+        }),
+        contentType: APPLICATION_TYPE.JSON,
+      })) as T;
   }
-
-  public async getGuildCommandPermissions(
+  public async getGuildCommandPermissions<T>(guildId: Snowflake): Promise<ApplicationCommandPermissions[]>;
+  public async getGuildCommandPermissions<T>(guildId: Snowflake): Promise<void>;
+  public async getGuildCommandPermissions<T>(
     guildId: Snowflake
-  ): Promise<ApplicationCommandPermissions[] | void> {
-    (await this.request.GET<ApplicationCommandPermissions[]>({
-      publicKey: true,
-      route: Routes.getGuildApplicationCommandPermissions(
-        this.options.botID,
-        guildId
-      ),
-      contentType: APPLICATION_TYPE.JSON,
-    })) as ApplicationCommandPermissions[];
+  ): Promise<T> {
+    return await Wrap(
+      this.request.GET<T>({
+        publicKey: true,
+        route: Routes.getGuildApplicationCommandPermissions(
+          this.options.botID,
+          guildId
+        ),
+        contentType: APPLICATION_TYPE.JSON,
+      })) as T;
   }
-
-  public async editGuildCommandPermissions(options: {
+  public async editGuildCommandPermissions<T>(options: { guildId: Snowflake; commandId: Snowflake; permissions: ApplicationCommandPermissions[]; }): Promise<ApplicationCommandPermissions>;
+  public async editGuildCommandPermissions<T>(options: { guildId: Snowflake; commandId: Snowflake; permissions: ApplicationCommandPermissions[]; }): Promise<void>;
+  public async editGuildCommandPermissions<T>(options: {
     guildId: Snowflake;
-    commandId: string;
+    commandId: Snowflake;
     permissions: ApplicationCommandPermissions[];
-  }): Promise<ApplicationCommandPermissions | void> {
-    (await this.request.PUT<ApplicationCommandPermissions>({
-      publicKey: true,
-      route: Routes.editApplicationCommandPermissions({
-        applicationId: this.options.botID,
-        guildId: options.guildId,
-        commandId: options.commandId,
-      }),
-      contentType: APPLICATION_TYPE.JSON,
-      data: options.permissions,
-    })) as ApplicationCommandPermissions;
+  }): Promise<T> {
+    return await Wrap(
+      this.request.PUT<T>({
+        publicKey: true,
+        route: Routes.editApplicationCommandPermissions({
+          applicationId: this.options.botID,
+          guildId: options.guildId,
+          commandId: options.commandId,
+        }),
+        contentType: APPLICATION_TYPE.JSON,
+        data: options.permissions,
+      })) as T;
   }
 }
